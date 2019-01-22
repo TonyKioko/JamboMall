bb1839ea0ea0ebcfb773811f379e0a1c0686a1bd
TonyKioko <tonykioko384@gmail.com>
2019-01-22 20:08:16 +0300

    context provider created

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 import Product from './Product';
+import Title from './Title';
+import {storeProducts} from '../data';
 
 export default class ProductList extends Component {
+    state ={
+        products:storeProducts
+    }
   render() {
+      console.log(storeProducts)
     return (
-      <div>
-        <p>Product List</p>
-        <Product>
-            
-        </Product>
-      </div>
+        <React.Fragment>
+            <div className="py-5">
+            <div className="container">
+                <div className="row">
+                <Title name="our" title="Products"></Title>
+                </div>
+            </div>
+            </div>
+        </React.Fragment>
+
+    //   <div>
+    //     <p>Product List</p>
+    //     <Product>
+
+    //     </Product>
+    //   </div>
     )
   }
 }
