7d53d16d81153de4ba07ef8735599338e535800b
TonyKioko <tonykioko384@gmail.com>
2019-01-26 00:02:45 +0300

    successfully adding $ removing from cart

--- src/context.js
+++ src/context.js
@@ -10,8 +10,11 @@ class ProductProvider extends Component {
     products:[],
     detailProduct,
     cart:[],
-    modalOpen:true,
-    modalProduct:detailProduct
+    modalOpen:false,
+    modalProduct:detailProduct,
+    cartSubTotal: 0,
+    cartTax: 0,
+    cartTotal: 0
 
   }
   componentDidMount() {
@@ -65,13 +68,114 @@ class ProductProvider extends Component {
       return {modalOpen:false}
     })
   }
+  increment = id => {
+    let tempCart = [...this.state.cart];
+    const selectedProduct = tempCart.find(item => {
+      return item.id === id;
+    });
+    const index = tempCart.indexOf(selectedProduct);
+    const product = tempCart[index];
+    product.count = product.count + 1;
+    product.total = product.count * product.price;
+    this.setState(() => {
+      return {
+        cart: [...tempCart]
+      };
+    }, this.addTotals);
+  };
+  decrement = id => {
+    let tempCart = [...this.state.cart];
+    const selectedProduct = tempCart.find(item => {
+      return item.id === id;
+    });
+    const index = tempCart.indexOf(selectedProduct);
+    const product = tempCart[index];
+    product.count = product.count - 1;
+    if (product.count === 0) {
+      this.removeItem(id);
+    } else {
+      product.total = product.count * product.price;
+      this.setState(() => {
+        return { cart: [...tempCart] };
+      }, this.addTotals);
+    }
+  };
+  getTotals = () => {
+    // const subTotal = this.state.cart
+    //   .map(item => item.total)
+    //   .reduce((acc, curr) => {
+    //     acc = acc + curr;
+    //     return acc;
+    //   }, 0);
+    let subTotal = 0;
+    this.state.cart.map(item => (subTotal += item.total));
+    const tempTax = subTotal * 0.1;
+    const tax = parseFloat(tempTax.toFixed(2));
+    const total = subTotal + tax;
+    return {
+      subTotal,
+      tax,
+      total
+    };
+  };
+  addTotals = () => {
+    const totals = this.getTotals();
+    this.setState(
+      () => {
+        return {
+          cartSubTotal: totals.subTotal,
+          cartTax: totals.tax,
+          cartTotal: totals.total
+        };
+      },
+      () => {
+        // console.log(this.state);
+      }
+    );
+  };
+  removeItem = id => {
+    let tempProducts = [...this.state.products];
+    let tempCart = [...this.state.cart];
+
+    const index = tempProducts.indexOf(this.getItem(id));
+    let removedProduct = tempProducts[index];
+    removedProduct.inCart = false;
+    removedProduct.count = 0;
+    removedProduct.total = 0;
+
+    tempCart = tempCart.filter(item => {
+      return item.id !== id;
+    });
+
+    this.setState(() => {
+      return {
+        cart: [...tempCart],
+        products: [...tempProducts]
+      };
+    }, this.addTotals);
+  };
+  clearCart = () => {
+    this.setState(
+      () => {
+        return { cart: [] };
+      },
+      () => {
+        this.setProducts();
+        this.addTotals();
+      }
+    );
+  };
   render() {
     return (
       <ProductContext.Provider value={{...this.state,
       handleDetail:this.handleDetail,
       addToCart:this.addToCart,
       openModal:this.openModal,
-      closeModal:this.closeModal
+      closeModal:this.closeModal,
+      increment: this.increment,
+      decrement: this.decrement,
+      removeItem: this.removeItem,
+      clearCart: this.clearCart
 
 
       
