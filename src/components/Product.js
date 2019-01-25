22c41bca6999bec24c80ef2960b81ce96bc58de2
TonyKioko <tonykioko384@gmail.com>
2019-01-25 15:15:41 +0300

    items adding to cart

--- src/components/Product.js
+++ src/components/Product.js
@@ -10,12 +10,22 @@ export default class Product extends Component {
     return (
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
       <div className="card">
+      <ProductConsumer>
+        {value => (
+          <div className="img-containainer p-5" onClick={() => value.handleDetail(id)}>
         
-        <div className="img-containainer p-5" onClick={() => console.log("Clicked")}>
+
         <Link to="/details">
         <img src={img} alt=""  className="card-img-top" />
         </Link>
-        <button className="cart-btn" disabled={inCart? true : false} onClick={() => console.log('Added to Cart')}>
+        <button
+                    className="cart-btn"
+                    disabled={inCart ? true : false}
+                    onClick={() => {
+                      value.addToCart(id);
+                      
+                    }}
+                  >
         {inCart ? (
                       <p className="text-capitalize mb-0" disabled>
                         in cart
@@ -26,6 +36,10 @@ export default class Product extends Component {
         
         </button>
         </div>
+        )}
+
+        </ProductConsumer>
+        
         <div className="card-footer d-flex justify-content-between">
             <p className="align-self-center mb-0">{title}</p>
             <h5 className="text-blue font-italic mb-0">
--- src/context.js
+++ src/context.js
@@ -8,7 +8,8 @@ const ProductContext = React.createContext()
 class ProductProvider extends Component {
   state = {
     products:[],
-    detailProduct
+    detailProduct,
+    cart:[]
   }
   componentDidMount() {
     this.setProducts()
@@ -24,19 +25,36 @@ class ProductProvider extends Component {
     })
   }
 
-
-  handleaddCart = () => {
-      console.log('Hello from add to cart')
+  getItem = id => {
+    const product = this.state.products.find(item => item.id === id);
+    return product
   }
-  handleDetail = () => {
-    console.log('Hello from handle detail')
+  addToCart = id => {
+      const tempProducts = [...this.state.products]
+      const index = tempProducts.indexOf(this.getItem(id))
+      const product = tempProducts[index]
+      product.inCart = true;
+      product.count = 1;
+      const price = product.price;
+      product.total = price;
+      this.setState(() => {
+        return {products:tempProducts,cart:[...this.state.cart,product]}
+      },() => {console.log(this.state)}
+      )
+
+  }
+  handleDetail = (id) => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return {detailProduct:product}
+    })
     
   }
   render() {
     return (
       <ProductContext.Provider value={{...this.state,
       handleDetail:this.handleDetail,
-      handleaddCart:this.addCart
+      addToCart:this.addToCart
 
       
       }}>
